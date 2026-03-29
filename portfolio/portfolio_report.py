7acb6f55c5c7c24f76e475d46bb9eea7c525ddc2
Dennis Zacharia <dennyzach15@gmail.com>
2026-03-29 11:59:08 -0400

    Update portfolio_report.py

--- portfolio/portfolio_report.py
+++ portfolio/portfolio_report.py
@@ -1,23 +1,17 @@
-import argparse
-import csv
 from collections import OrderedDict
-
+import csv
 
 def read_portfolio(filename):
     data = []
-
     with open(filename, "r") as file:
         reader = csv.DictReader(file)
-
         for row in reader:
             item = OrderedDict()
             item["symbol"] = row["symbol"]
             item["units"] = row["units"]
             item["cost"] = row["cost"]
             data.append(item)
-
     return data
-
 
 def save_portfolio(data, filename):
     with open(filename, "w", newline="") as file:
